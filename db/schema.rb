4e592bc297ec34c4b06163e086543e0880ca12c9
masayaono <73627025+masayaono@users.noreply.github.com>
2020-12-16 15:19:01 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_034051) do
     t.string "family_name", null: false
     t.string "first_name_kana", null: false
     t.string "family_name_kana", null: false
-    t.string "birth_strftime", null: false
+    t.date "birth_strftime", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
