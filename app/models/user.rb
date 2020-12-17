5ce72bb025a797899d9002d3f08e40bb6a07f96e
masayaono <73627025+masayaono@users.noreply.github.com>
2020-12-17 17:42:55 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,19 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  with_options presence: true do
-  validates :birth_strftime
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  
   validates :password, length: { minimum: 6 },
-                      format: { with: VALID_PASSWORD_REGEX }
-  validates :nickname
-  validates :first_name
-  validates :family_name
-  validates :first_name_kana
-  validates :family_name_kana
+                       format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  with_options presence: true do
+    validates :birth_strftime
+    validates :password
+    validates :nickname
+    validates :first_name
+    validates :family_name
+    validates :first_name_kana
+    validates :family_name_kana
   end 
-  with_options format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+  with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
     validates :first_name
     validates :family_name
   end
